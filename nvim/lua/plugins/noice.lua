69d04f5e21297c5ad446353748960179c2698140
Jeffrey Layton <jeffreylayton@macbookpro.mynetworksettings.com>
2024-10-02 11:47:24 -0700

    removed ds store

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,39 @@
+return {
+	"folke/noice.nvim",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+	config = function()
+		require("noice").setup({
+			lsp = {
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signature help
+			},
+		})
+		require("notify").setup({
+			background_colour = "#1E1E2E",
+			stages = "fade",
+			timeout = 3000,
+			max_width = 120,
+			icons = {
+				ERROR = "",
+				WARN = "",
+				INFO = "",
+				DEBUG = "",
+				TRACE = "✎",
+			},
+		})
+	end,
+}
