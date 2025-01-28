db6c8a87174ddcf4917a0ce24352d6c9b944a9ca
Jeffrey Layton <jeffreylayton@macbookpro.mynetworksettings.com>
2025-01-27 23:15:14 -0700

    nvim: updated marview and added more mini | ghostty: switched from wezterm

--- nvim/lua/jeffreylayton/mappings.lua
+++ nvim/lua/jeffreylayton/mappings.lua
@@ -3,8 +3,8 @@ vim.keymap.set({ "i", "v" }, "KJ", "<Escape>")
 
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
+-- vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
+-- vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<leader>fm", ":FormatWrite<CR>")
 
--- nvim/lua/plugins/markview.lua
+++ nvim/lua/plugins/markview.lua
@@ -2,9 +2,11 @@ return {
 	"OXY2DEV/markview.nvim",
 	lazy = false, -- Recommended
 	-- ft = "markdown" -- If you decide to lazy-load anyway
-
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 		"echasnovski/mini.icons",
 	},
+	config = function()
+		require("markview").setup()
+	end,
 }
