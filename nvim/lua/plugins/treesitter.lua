a7c487a76fb266cc7ac7df27aa16fe80981e751f
Jeffrey Layton <jeffreylayton@macbookpro.mynetworksettings.com>
2025-03-11 19:40:29 -0700

    ghostty: adjusted split keybinds

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -33,7 +33,7 @@ return {
 			highlight = {
 				enable = true,
 				disable = {},
-				additional_vim_regex_highlighting = false,
+				additional_vim_regex_highlighting = true,
 			},
 			indent = { enable = true },
 			autotag = {
