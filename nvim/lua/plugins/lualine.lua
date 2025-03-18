3a25f984a5d05ad86a2ae2e4f0f3716a61cda79f
Jeffrey Layton <jeffreylayton@macbookpro.mynetworksettings.com>
2025-03-18 11:12:45 -0700

    feat(nvim.lualine): improved lualine config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,8 +1,36 @@
-local function getWords()
+local function root_dir()
+	local function get_project_root()
+		local markers = { ".git", "package.json", "Makefile", "README.md" }
+		local cwd = vim.loop.cwd()
+		for _, marker in ipairs(markers) do
+			local found = vim.fs.find(marker, { path = cwd, upward = true, limit = 1 })
+			if #found > 0 then
+				return vim.fs.dirname(found[1])
+			end
+		end
+		return cwd
+	end
+
+	local root = get_project_root()
+	if not root then
+		return ""
+	end
+
+	local home = os.getenv("HOME")
+	if home and root:find(home, 1, true) == 1 then
+		root = "~" .. root:sub(#home + 1)
+	end
+
+	return root
+end
+
+local function get_words()
 	if vim.fn.mode() == "v" or vim.fn.mode() == "V" or vim.fn.mode() == "" then
 		return tostring(vim.fn.wordcount().visual_words .. " words")
-	else
+	elseif vim.bo.filetype == "text" or vim.bo.filetype == "markdown" then
 		return vim.fn.wordcount().words .. " words"
+	else
+		return ""
 	end
 end
 
@@ -16,27 +44,32 @@ return {
 			options = {
 				theme = "catppuccin",
 				icons_enabled = true,
-				component_separators = "",
-				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "│" },
+				section_separators = { left = "", right = "" },
 			},
 			sections = {
-				lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
-				lualine_b = { "filename", "branch" },
+				lualine_a = {
+					{ "mode", icon = "" },
+				},
+				lualine_b = {
+					{ root_dir, icon = "󱉭" },
+				},
 				lualine_c = {
-					"%=", --[[ add your center compoentnts here in place of this comment ]]
+					{
+						"branch",
+						fmt = function(str)
+							local branch = str
+							if string.len(str) > 32 then
+								branch = string.sub(str, 1, 17) .. "…" .. string.sub(str, -13)
+							end
+
+							return branch
+						end,
+						icon = "",
+					},
 				},
-				lualine_x = {},
-				lualine_y = { "filetype", getWords, "location" },
-				lualine_z = {
-					{ "progress", separator = { right = "" }, left_padding = 2 },
-				},
-			},
-			inactive_sections = {
-				lualine_a = { "filename" },
-				lualine_b = {},
-				lualine_c = {},
-				lualine_x = {},
-				lualine_y = {},
+				lualine_x = { "filetype" },
+				lualine_y = { "diagnostics", "diff", get_words },
 				lualine_z = { "location" },
 			},
 		})
