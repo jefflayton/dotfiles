b11e942bb87a93b92ea9b591dd1a1a3067965552
Jeffrey Layton <jefflayton23@icloud.com>
2025-05-20 17:50:12 -0700

    feat(nvim.git): added keymaps for Diffview

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,5 +1,19 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	"tpope/vim-fugitive",
-	"sindrets/diffview.nvim",
+	{
+		"sindrets/diffview.nvim",
+		keys = {
+			{
+				"<leader>gd",
+				"<CMD>DiffviewOpen<CR>",
+				desc = "Git: Diffview Open",
+			},
+			{
+				"<leader>gdc",
+				"<CMD>DiffviewClose<CR>",
+				desc = "Git: Diffview Close",
+			},
+		},
+	},
 }
