5dec635c046549714aba7cb24c9a9dbf2ae9617a
Jeff Layton <jefflayton23+github@icloud.com>
2025-08-25 16:23:06 -0700

    updated tmux and nvim

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -1,21 +1,70 @@
+local validate = vim.validate
+local nvim_eleven = 1
+
 local M = {}
 
-function M.deno_or_node(deno, node)
-	local fname = vim.api.nvim_buf_get_name(0)
+local function escape_wildcards(path)
+	return path:gsub("([%[%]%?%*])", "\\%1")
+end
 
-	local deno_wd = nil
-	if vim.uv.fs_stat(fname) ~= nil then
-		deno_wd = vim.fs.dirname(vim.fs.find({ "deno.json", "deno.jsonc" }, { path = fname, upward = true })[1])
-	end
+function M.root_pattern(...)
+	local patterns = M.tbl_flatten({ ... })
+	return function(startpath)
+		startpath = M.strip_archive_subpath(startpath)
+		for _, pattern in ipairs(patterns) do
+			local match = M.search_ancestors(startpath, function(path)
+				for _, p in ipairs(vim.fn.glob(table.concat({ escape_wildcards(path), pattern }, "/"), true, true)) do
+					---@diagnostic disable-next-line: undefined-field
+					if vim.uv.fs_stat(p) then
+						return path
+					end
+				end
+			end)
 
-	if deno_wd ~= nil then
-		return deno
-	else
-		return node
+			if match ~= nil then
+				return match
+			end
+		end
 	end
 end
 
--- From nvim.lspconfig.util
+function M.tbl_flatten(t)
+	--- @diagnostic disable-next-line:deprecated
+	return nvim_eleven and vim.iter(t):flatten(math.huge):totable() or vim.tbl_flatten(t)
+end
+
+function M.search_ancestors(startpath, func)
+	if nvim_eleven then
+		validate("func", func, "function")
+	end
+	if func(startpath) then
+		return startpath
+	end
+	local guard = 100
+	for path in vim.fs.parents(startpath) do
+		-- Prevent infinite recursion if our algorithm breaks
+		guard = guard - 1
+		if guard == 0 then
+			return
+		end
+
+		if func(path) then
+			return path
+		end
+	end
+end
+
+function M.strip_archive_subpath(path)
+	-- Matches regex from zip.vim / tar.vim
+	path = vim.fn.substitute(path, "zipfile://\\(.\\{-}\\)::[^\\\\].*$", "\\1", "")
+	path = vim.fn.substitute(path, "tarfile:\\(.\\{-}\\)::.*$", "\\1", "")
+	return path
+end
+
+function M.insert_package_json(root_files, field, fname)
+	return M.root_markers_with_field(root_files, { "package.json", "package.json5" }, field, fname)
+end
+
 --- Appends `new_names` to `root_files` if `field` is found in any such file in any ancestor of `fname`.
 ---
 --- NOTE: this does a "breadth-first" search, so is broken for multi-project workspaces:
@@ -42,8 +91,14 @@ function M.root_markers_with_field(root_files, new_names, field, fname)
 	return root_files
 end
 
-function M.insert_package_json(root_files, field, fname)
-	return M.root_markers_with_field(root_files, { "package.json", "package.json5" }, field, fname)
+function M.deno_or_node(deno, node)
+	local deno_wd = M.root_pattern({ "deno.json", "deno.jsonc" })()
+
+	if deno_wd ~= nil then
+		return deno
+	else
+		return node
+	end
 end
 
 return M
---
+++ nvim/snippets/zig.lua
@@ -0,0 +1,10 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local d = ls.dynamic_node
+local sn = ls.snippet_node
+local fmta = require("luasnip.extras.fmt").fmta
+
+return {}
